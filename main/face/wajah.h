a25a1e5f3e1e0f511339515cc990fdbb7a88fc83
abdulhakim84 <54586446+abdulhakim84@users.noreply.github.com>
2026-07-04 23:11:24 +0700

    Update wajah.h

--- main/face/wajah.h
+++ main/face/wajah.h
@@ -5,18 +5,14 @@
 enum RobotMode { MODE_FACE };
 enum Emotion { EMOT_BIASA, EMOT_SENANG, EMOT_MARAH, EMOT_SEDIH, EMOT_NGANTUK };
 
-// Deklarasi variabel emosi global
+// Deklarasi variabel emosi global agar bisa diubah dari application.cc
 extern Emotion currentEmotion;
 
-// Deklarasi fungsi standar
+// Deklarasi fungsi standar wajah
+void initFaceDisplay();
 void updateFacePhysics();
 void updateLook();
 void updateBlink();
-
-#ifdef __cplusplus
-// Kita gunakan forward declaration (void*) atau class pointer agar tidak perlu include SSD1306 di header
-class Adafruit_SSD1306;
-void drawFace(Adafruit_SSD1306 &display);
-#endif
+void renderFaceLoop();
 
 #endif // WAJAH_H
