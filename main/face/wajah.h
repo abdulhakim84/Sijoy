248b7a8056fe617ad4b3b04b440e9d39cae01992
abdulhakim84 <54586446+abdulhakim84@users.noreply.github.com>
2026-07-04 22:57:55 +0700

    Refactor wajah.h to clean up includes
    Removed unnecessary includes and added forward declaration for Adafruit_SSD1306.

--- main/face/wajah.h
+++ main/face/wajah.h
@@ -1,16 +1,11 @@
 #ifndef WAJAH_H
 #define WAJAH_H
 
-#include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
 // Definisikan tipe data Enum secara global
 enum RobotMode { MODE_FACE };
 enum Emotion { EMOT_BIASA, EMOT_SENANG, EMOT_MARAH, EMOT_SEDIH, EMOT_NGANTUK };
 
-// Deklarasi variabel emosi global agar bisa diubah dari application.cc
+// Deklarasi variabel emosi global
 extern Emotion currentEmotion;
 
 // Deklarasi fungsi standar
@@ -19,6 +14,8 @@ void updateLook();
 void updateBlink();
 
 #ifdef __cplusplus
+// Kita gunakan forward declaration (void*) atau class pointer agar tidak perlu include SSD1306 di header
+class Adafruit_SSD1306;
 void drawFace(Adafruit_SSD1306 &display);
 #endif
 
